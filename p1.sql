99e08ba2ec5b70198236d9b3ce361c9a56212974
mvanbraeckel <mvanbrae@uoguelph.ca>
2020-11-11 02:56:27 -0500

    fixed assumptions, upper p1 table return col vals, adding p8

--- p1.sql
+++ p1.sql
@@ -4,7 +4,7 @@
  *  Write the program as a function that accepts a customer name as a parameter, and displays transactions of the customer. (1%)
  */
 
-create or replace function q01(name char) returns table (vendor_name char(20), trans_date date, trans_amount numeric(10,2)) as $$
+create or replace function q01(name char) returns table (vendor_name CHAR(20), trans_date DATE, trans_amount NUMERIC(10,2)) as $$
     begin
         return query
         select vname, t_date, amount
